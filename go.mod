7fc348a58af587da67bdc099cceed27b1b23aa0e
lackone <lackone@126.com>
2023-03-22 17:31:18 +0800

    添加

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lackone/go-epoll
+
+go 1.20
---
+++ utils.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net"
+	"reflect"
+)
+
+// 获取连接的FD
+func GetSocketFD(ln interface{}) (int, error) {
+	//通过反射，获取类型
+	t := reflect.Indirect(reflect.ValueOf(ln)).Type().String()
+	switch t {
+	case "net.TCPConn":
+		//通过反射，获取net.TCPConn结构体中的conn字段
+		conn := reflect.Indirect(reflect.ValueOf(ln)).FieldByName("conn")
+		return ValueTCPFD(conn), nil
+	case "net.TCPListener":
+		return PointerTCPFD(ln), nil
+	case "tls.Conn":
+		conn := reflect.Indirect(reflect.ValueOf(ln)).FieldByName("conn")
+		//注意，这里使用Elem()获取指针变量指向的值，因为tls.Conn结构体conn的类型为net.Conn，是一个interface
+		//所以，比net.TCPConn多一步
+		conn = reflect.Indirect(conn.Elem())
+		return ValueTCPFD(conn), nil
+	case "tls.listener":
+		tln := reflect.Indirect(reflect.ValueOf(ln)).FieldByName("Listener")
+		if ln, ok := tln.Interface().(*net.TCPListener); ok {
+			return PointerTCPFD(ln), nil
+		}
+	}
+	return 0, errors.New(fmt.Sprintf("get socket fd error type : %s", t))
+}
+
+func PointerTCPFD(ln interface{}) int {
+	fdVal := reflect.Indirect(reflect.ValueOf(ln)).FieldByName("fd")
+	pfdVal := reflect.Indirect(fdVal).FieldByName("pfd")
+	return int(pfdVal.FieldByName("Sysfd").Int())
+}
+
+func ValueTCPFD(ln reflect.Value) int {
+	fdVal := ln.FieldByName("fd")
+	pfdVal := reflect.Indirect(fdVal).FieldByName("pfd")
+	return int(pfdVal.FieldByName("Sysfd").Int())
+}
