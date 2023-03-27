c9b4935a7f65a7b0b95d4260f2367a5b40ea2a9a
lackone <lackone@126.com>
2023-03-27 14:42:14 +0800

    修改epoll

--- example/main.go
+++ example/main.go
@@ -16,7 +16,7 @@ func (h *Handler) OnConnect(conn *go_epoll.Conn) {
 }
 
 func (h *Handler) OnData(conn *go_epoll.Conn) {
-	//conn连接实现了Read()和Write()方法
+	//conn连接实现了Read()和Write()方法，你也可以添加自已的encode和decode方法。
 	//没有数据了，一定要退出for循环，如果有数据来了，事件反应堆会回调一次OnData方法，如果多个OnData一直都没退出，会出问题。
 	reader := bufio.NewReader(conn)
 	for {
